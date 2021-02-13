6a0915d35787c333860b454617629467c3431470
Roman Bar <barom64@mail.ru>
2021-02-13 14:49:45 +0400

    2. Создать переменные всех пройденных типов данных и инициализировать их значения.

---
+++ practic1/src/upr2.java
@@ -0,0 +1,16 @@
+public class upr2 {
+
+    public static void main(String[] args)
+    {
+        int a = 1000;
+        float b = 2.5f;
+        byte c = -3;
+        short d = 6;
+        long e = 121352121L;
+        double f = 4656.656;
+        char g = 'i';
+        boolean h = true;
+
+
+    }
+}
